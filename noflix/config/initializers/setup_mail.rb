d71e3ab9d9625a07258793741a630f8e79cb503b
Raimundo Herrera <rjherrera@uc.cl>
2017-06-28 18:17:56 -0400

    mailing fixes

--- noflix/config/initializers/setup_mail.rb
+++ noflix/config/initializers/setup_mail.rb
@@ -1,11 +1,10 @@
 ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
     :user_name => 'app67577396@heroku.com',
     :password => 'boq9nmdo6540',
-    :domain => 'noflix.herokuapp.com',
+    :domain => 'heroku.com',
     :enable_starttls_auto => true
 }
